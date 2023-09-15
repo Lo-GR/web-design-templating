ca1005b9bf1c47de89b11e334c487ec2577214e4
Zizzs <Zizzs17@gmail.com>
2023-09-15 13:14:12 -0700

    Fixed

--- src/Components/Heros/HeroOne/index.tsx
+++ src/Components/Heros/HeroOne/index.tsx
@@ -14,9 +14,9 @@ export const Hero = ({
   backgroundImage,
 }: HeroProps) => {
   return (
-    <div className="relative left-0 right-0">
+    <div className="relative">
       <img className="absolute top-0" src={backgroundImage} />
-      <div className="absolute container p-5 flex flex-col items-center top-32 w-[800px]">
+      <div className="relative container p-5 flex flex-col items-center top-16 justify-center mx-auto">
         <p className="text-[87px]">{title}</p>
         <p className="text-[33px]  py-5">{text}</p>
         <Button>{buttonText}</Button>
