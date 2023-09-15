8ca5240cc0f4d0f1f75b325ac1c7c099a7175faa
Zizzs <Zizzs17@gmail.com>
2023-09-15 12:38:52 -0700

    Tailwind configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const Layout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    //errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+        //errorElement: <ErrorPage />,
+      },
+    ],
+  },
+]);
+
+root.render(<RouterProvider router={router} />);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sp-primary': {
+          200: '00B5AB',
+          300: '#00B5B1',
+          400: '#00B3B5',
+          500: '#00ADB5',
+          600: '#00A7B5',
+          700: '#00A1B5',
+        },
+        'sp-secondary': {
+          300: '#de7c78',
+          500: '#e0615c',
+        },
+        'sp-tertiary': {
+          500: '#FF3E36',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
